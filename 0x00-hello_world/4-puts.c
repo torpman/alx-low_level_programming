dd308a5e626e86d6de21ff44d7ac5a117bc326d6
torpman <torpman1112@gmail.com>
2022-03-10 14:46:24 -0800

    type size added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-/** 
+/**
  * main: Entry point
  *
- * return: always return 0 on success
+ * Return: Always 0 (success)
  */
 int main(void)
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
