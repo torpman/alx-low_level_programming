b5aa47b2684625e6c1786fb1c50b86dc75f95353
torpman <torpman1112@gmail.com>
2022-03-10 14:21:07 -0800

    debugging puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-int main()
+/** 
+ * main: Entry point
+ *
+ * return: always return 0 on success*/
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
