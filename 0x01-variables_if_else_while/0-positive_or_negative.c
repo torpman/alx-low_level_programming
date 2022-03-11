48ade3f0eb183fff3cc872f6a16a670873dcf83f
torpman <torpman1112@gmail.com>
2022-03-11 11:55:46 -0800

    detected a postive and negative number from random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,20 +1,20 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
 /**
- * main - this is the entry point where the code execution begin
+ *main - this is the entry point where the code execution begin
  *
- * Description: this code output positive or negative 
- * base on a random number generated
+ *Description: this code output positive or negative
+ *base on a random number generated
  *
- * Return: return 0 on successful execution
+ *Return: return 0 on successful execution
  */
 
 int main(void)
 {
-	
+
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 /* your code goes there */
