c2da8587e287e461834cd78da99d6bcd1aa0f8c9
torpman <torpman1112@gmail.com>
2022-03-10 17:36:55 -0800

    advnaced task added

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - the entry point where the execution
+  * start
+  *
+  * Return: the function return 1 (Success)
+  */
+
+int main(void)
+{
+	
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
+
