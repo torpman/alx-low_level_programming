21ea2406a64df2c12cc783d76af4b71809aa54be
torpman <torpman1112@gmail.com>
2022-03-11 17:26:36 -0800

    reverse alphabet printed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio>
+
+/**
+  * main - entry point
+  *
+  * Description: the code snipet print a - z in reverse order
+  *
+  * Return: return 0 on successful execution
+  */
+
+int main(void)
+{
+
+	char re = 'z';
+
+	while (re >= 'a')
+	{	
+		putchar(re);
+		re--;
+	}
+	putchar('\n');
+	return (0);
+
+}
