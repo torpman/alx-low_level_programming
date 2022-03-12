3e0966c49a850ac6a49e3e54639b1eea372506a2
torpman <torpman1112@gmail.com>
2022-03-11 18:36:13 -0800

    mmessage

---
+++ 0x01-variables_if_else_while/101-print_combo4.c
@@ -0,0 +1,35 @@
+#include <stdio>
+
+/**
+ * main - entry point
+ * Description: comb4
+ * Return: return 0
+ */
+
+int main(void)
+{
+	in i, j, k, l;
+
+	for (i = 0; i < 1000; i++)
+	{
+		j = i / 100; /* hundreds */
+		k = i / 10; /* tens*/
+		l = i % 10;
+
+		if (j < k && k < l)
+		{
+			putchar(j + '0');
+			putchar(k + '0');
+			putchar(l + '0');
+
+			if (i < 700)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
