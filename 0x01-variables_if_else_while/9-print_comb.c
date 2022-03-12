2622816bdd70cd888a47333c9466ade60cde09a6
torpman <torpman1112@gmail.com>
2022-03-11 17:59:26 -0800

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * mian - entry point
+ * Description: Print all possible combination for a siongle digit numbers
+ * return: 0
+ */
+
+int main(void)
+{
+	int = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if(i < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
