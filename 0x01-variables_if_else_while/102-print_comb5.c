6cf0fbf736558035a753f689accde4cde472161d
torpman <torpman1112@gmail.com>
2022-03-12 11:35:38 -0800

    new file added

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio>
+
+/**
+ * main - entry point of the execution
+ *
+ * Description: the code print some combo of numbers
+ *
+ * Return: return 0 on successful execution
+ */
+int main(void)
+{
+	int i, j, a, b, c, d;
+
+	for (i = 0; i < 100; i++)
+	{
+		a = i / 10; /* doubles first num*/
+		b = i % 10; /* singles first num*/
+
+		for (j = 0; j < 100; j++)
+		{
+			c = j / 10; /* doubles second number*/
+			d = j % 10; /* singles second number*/
+
+			if (a < c || (a == c && b < d))
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
+
+				if (!(a == 9 && b == 8))
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
