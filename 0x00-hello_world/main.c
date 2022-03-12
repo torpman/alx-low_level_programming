fd2bc3fca3ec4ac5bff3cfab3cdfe535a61f9724
torpman <torpman1112@gmail.com>
2022-03-12 11:10:43 -0800

    debugged intel  assembly code

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,14 @@
+#include <stdio>
+
+/**
+ * main - entry point
+ *
+ * Description: the code  njust return
+ *
+ * Return: return 0 on successful execution
+ */
+
+int main(void)
+{
+	return (0);
+}
