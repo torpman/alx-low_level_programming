024a064b1d61d7fc6ecdaebc7382694ffddb04d2
torpman <torpman1112@gmail.com>
2022-03-15 19:16:45 -0700

    debugging

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,7 +18,7 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n);
 			n--;
-			if (n == 98)
+			if (n < 98)
 			{
 				break;
 			}
@@ -30,7 +30,7 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n);
 			n++;
-			if (n == 98)
+			if (n > 98)
 			{
 				break;
 			}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,23 @@
 #include "main.h"
 
 /**
- * print_last_digit - this function prints the last digit of a number
- * @num: the number inputted
- * Return: return the last digit on successful
+ * print_last_digit - prints the last digit of a number
+ * @n: the int to extract the last digit from
+ * Return: value of the last digit
  */
-
-int print_last_digit(int num)
+int print_last_digit(int n)
 {
 	int a;
-	a = num % 10;
+
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
 
 	return (a);
 }
