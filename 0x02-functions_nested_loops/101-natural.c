1cb01aa8ec1a42538f93298ce32bf146e22c00ec
torpman <torpman1112@gmail.com>
2022-03-15 20:08:36 -0700

    natuiral numbes

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - computes and prints the sum of all the multiples
+ * of 3 or 5 below 1024
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long int s3, s5, s;
+	int i;
+
+	s3 = 0;
+	s5 = 0;
+	s = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			sum3 = sum3 + i;
+		} else if ((i % 5) == 0)
+		{
+			sum5 = sum5 + i;
+		}
+	}
+	sum = sum3 + sum5;
+	printf("%lu\n", sum);
+	return (0);
+}
