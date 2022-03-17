247f4f85584af3a63e503b71523b294c37a79f3a
torpman <torpman1112@gmail.com>
2022-03-17 10:13:25 -0700

    print number from 0-9 added

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - this function print number 0-9
+ */
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
