9a08be377ebbb98b4b12c60bb54eebee6265b62f
torpman <torpman1112@gmail.com>
2022-03-17 11:23:41 -0700

    upper debugged

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -11,7 +11,7 @@
 
 int _isupper(int c)
 {
-	if (c > 64 && c < 91)
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - prints out 0-14 ten times
+ */
+
+void more_numbers(void)
+{
+
+	int i;
+	char j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = "0"; j < "15"; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+
+}
