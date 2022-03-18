2db055e13847b2404a244ef859b171a073fbdcf4
torpman <torpman1112@gmail.com>
2022-03-18 16:29:17 -0700

    lines printing

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,15 +7,18 @@
 
 void print_line(int n)
 {
+	int i;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (int i = 0; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}
 	}
+	_putchar('\n');
 }
