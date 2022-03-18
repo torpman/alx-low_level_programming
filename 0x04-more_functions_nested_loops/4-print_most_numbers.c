6bf863c891cea9b8d8341f4686305534ba64c158
torpman <torpman1112@gmail.com>
2022-03-18 15:25:00 -0700

    printing most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,10 +7,10 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = "0"; i < "10"; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if !(i == "2" || i == "4")
-			_putchar(i + '0');
+		if !(i == '2' && i == '4')
+			_putchar(i);
 	}
 	_putchar('\n');
 }
