23d3d9198f6fdebb728f0310205ad12008fba493
torpman <torpman1112@gmail.com>
2022-03-18 16:10:25 -0700

    printing 1-14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,17 +5,28 @@
 
 void more_numbers(void)
 {
-
 	int i, j;
+	char c;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 15; j++)
+		c = '0';
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j);
+			if (j > 9)
+			{
+				_putchar('1');
+			}
+			_putchar(c);
+			if (c == '9')
+			{
+				c = '0';
+			}
+			else
+			{
+				c++;
+			}
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
-
 }
