83ddf4216bfcadb0427584682320a123ace3b7d8
torpman <torpman1112@gmail.com>
2022-03-18 17:38:12 -0700

    coding my way out of wet paper bag

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,12 +13,18 @@ int main(void)
 	for (num = 1; num <= 100; num++)
 	{
 		if (num % 3 == 0)
+		{
 			printf("Fizz ");
+		}
 		else if (num % 5 == 0)
+		{
 			printf("Buzz ");
+		}
 		else if ((num % 5) == 0 && (num % 3) == 0)
+		{
 			printf("FizzBuzz ");
-		printf("%d", num);
+		}
+		printf("%d ", num);
 	}
 	_putchar('\n');
 }
