dfdaf496a96020a3954d999962e23d3fdf2edcaf
torpman <torpman1112@gmail.com>
2022-03-18 17:34:26 -0700

    coding my way out of wet paper

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - the entry point
+ * Return: return 0 on successful execution
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0)
+			printf("Fizz ");
+		else if (num % 5 == 0)
+			printf("Buzz ");
+		else if ((num % 5) == 0 && (num % 3) == 0)
+			printf("FizzBuzz ");
+		printf("%d", num);
+	}
+	_putchar('\n');
+}
