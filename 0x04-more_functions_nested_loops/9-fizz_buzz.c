ecc2632dbeafd0c0f552a882bc65034852223652
torpman <torpman1112@gmail.com>
2022-03-18 17:57:52 -0700

    coding my way out oif wet paper bag

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,23 @@ int main(void)
 	{
 		if (num % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (num % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if ((num % 5) == 0 && (num % 3) == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
-			printf("%d ", num);
+			printf("%d", num);
+		}
+		if (num < 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
