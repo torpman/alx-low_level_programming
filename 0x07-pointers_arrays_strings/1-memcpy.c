740887cbf1ffeabe397c12b6a7278671aa74d6f3
torpman <torpman1112@gmail.com>
2022-03-28 16:25:44 -0700

    strspn added

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -15,9 +15,8 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; src[i] && i < n; i++)
-	{
-		dest[i] = src[i];
-	}
+	for (i = 0; i < n; i++)
+		*(dest + i) =  *(src + i);
+
 	return (dest);
 }
