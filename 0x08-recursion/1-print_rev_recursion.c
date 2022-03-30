4dd1b5725750b102f29afb7db3211f8cd0f900a8
torpman <torpman1112@gmail.com>
2022-03-30 16:03:21 -0700

    reverse strig recursively

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion - this recursive function prints string in reverse
+*
+* @s: the string to reverse
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
