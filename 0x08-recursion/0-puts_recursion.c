5b5814e4f06289b70c918b4608e8a903f41f80d9
torpman <torpman1112@gmail.com>
2022-03-30 15:44:07 -0700

    puts operation performed recursively

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _puts_recursion - this is a recursive fucntion 
+* that prints a string followed by a new line
+*
+* @s: the string to print out
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This directory houses everything about recursion
