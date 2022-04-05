a4270d1e30b01259da66aa16d6501236b78ea592
torpman <torpman1112@gmail.com>
2022-04-05 16:29:32 -0700

    memory freed after use

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,13 +14,16 @@ int **alloc_grid(int width, int height)
 {
 	int i, j;
 	int **arr;
-	
+
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
 	arr = malloc(sizeof(int *) * height);
 	if (arr == NULL)
+	{
+		free(arr);
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
@@ -28,6 +31,9 @@ int **alloc_grid(int width, int height)
 		arr[i] = malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
 		{
+			for (i--; i >= 0; i--)
+				free(arr[i]);
+			free(arr);
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
