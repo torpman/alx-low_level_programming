aa79efa59cd15fab231caf629cee615c8109bb29
torpman <torpman1112@gmail.com>
2022-04-04 19:01:13 -0700

    string concatenate added

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+* *str_concat - This function concatenate two strings
+*
+* @s1: the first string
+* @s2: the second string
+*
+* Return: return a pointer to the concatenated string memory location
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	char *strout;
+	unsigned int i, j, k, limit;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	strout = malloc(sizeof(char) * (i + j + 1));
+
+	if (strout == NULL)
+	{
+		free(strout);
+		return (NULL);
+	}
+
+	for (k = 0; k < i; k++)
+		strout[k] = s1[k];
+
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
+		strout[k] = s2[j];
+
+	return (strout);
+}
