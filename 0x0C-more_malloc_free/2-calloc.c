878d492d42de040995f77583f50ff70a4109cd74
torpman <torpman1112@gmail.com>
2022-04-07 19:06:18 -0700

    array memomory allocated

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *mem;
-	int i;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -23,7 +23,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (mem == NULL)
 		return (NULL);
 
-	for (i = 0; mem[i]; i++)
+	for (i = 0; i < nmemb; i++)
 		mem[i] = 0;
 
 	return (mem);
