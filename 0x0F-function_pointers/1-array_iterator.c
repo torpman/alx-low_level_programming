1f7ac4bcfc9258ccd45fae6021db8b5e591209b8
torpman <torpman1112@gmail.com>
2022-04-13 06:17:07 -0700

    array iterated with pointer to function as argument

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+* array_iterator - This function iterate through an array and print
+* its elements
+*
+* @array: The array to iterate through
+* @size: The size of the array
+* @action: The pointer that handles the printing out of the element
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
