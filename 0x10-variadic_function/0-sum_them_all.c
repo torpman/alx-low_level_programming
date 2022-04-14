d1624178436ac440d5db810d8b1ab242e4b81a8f
torpman <torpman1112@gmail.com>
2022-04-14 10:16:01 -0700

    all integer argument passed added

---
+++ 0x10-variadic_function/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+* sum_them_all - This function sum all the argument passed into it
+*
+* @n: The number of argument passed
+*
+* Return: Return the total summation of the passed integer numbers
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	unsigned int sum = 0;
+	va_list arg_pnt;
+
+	va_start(arg_pnt, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(arg_pnt, int);
+	}
+
+	va_end(arg_pnt);
+
+	return (sum);
+}
---
+++ 0x10-variadic_function/variadic_functions.h
@@ -0,0 +1,6 @@
+#ifndef _VARIADIC_FUNCTION_H_
+#define _VARIADIC_FUNCTION_H_
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
