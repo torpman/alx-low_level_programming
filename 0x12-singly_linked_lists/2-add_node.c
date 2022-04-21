9a0e00948ecdd8008b026164063a156b01c986e6
torpman <torpman1112@gmail.com>
2022-04-21 08:45:32 -0700

    final debugging to adding node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,6 +5,8 @@
 *
 * @head: The list in which new node is added
 * @str: the string to be added to the new node
+*
+* Return: return a pointer to the address of the new list
 */
 
 list_t *add_node(list_t **head, const char *str)
@@ -14,8 +16,10 @@ list_t *add_node(list_t **head, const char *str)
 
 	i = 0;
 	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-	while (str[i] != '\0')	
+	while (str[i] != '\0')
 		i++;
 
 	new_node->str = strdup(str);
