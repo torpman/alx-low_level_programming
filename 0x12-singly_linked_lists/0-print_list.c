6156cb5cf048662c12075d47f6095d13e2c41ec4
torpman <torpman1112@gmail.com>
2022-04-21 15:51:13 -0700

    print list fixed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,7 @@
 
 size_t print_list(const list_t *h)
 {
-	int count;
+	size_t count;
 
 	count = 0;
 	while (h != NULL)
@@ -17,9 +17,9 @@ size_t print_list(const list_t *h)
 		count++;
 
 		if (h->str == NULL)
-			printf("[%d] %s", 0, "(nil)");
-
-		printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 
 	}
