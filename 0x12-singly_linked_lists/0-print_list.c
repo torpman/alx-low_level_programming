3545337ee11fd3149c5d365f7e4a27f09415f1a9
torpman <torpman1112@gmail.com>
2022-04-21 08:27:47 -0700

    list printing debugged

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,13 +14,14 @@ size_t print_list(const list_t *h)
 	count = 0;
 	while (h != NULL)
 	{
+		count++;
+
 		if (h->str == NULL)
 			printf("[%d] %s", 0, "(nil)");
 
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 
-		count++;
 	}
 
 
