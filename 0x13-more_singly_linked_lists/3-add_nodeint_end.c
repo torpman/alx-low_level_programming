6f7473816d5e154d4d5eae0585fd5e3fa47efd5b
torpman <torpman1112@gmail.com>
2022-04-25 14:57:42 -0700

    updated header file

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,15 +18,18 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	/*create new node*/
 	new_node->n = n;
+	new_node->next = NULL;
 
 	if (*head == NULL)
 		*head = new_node;
 	else
 	{
 		last_node = *head;
+
 		while (last_node->next != NULL)
 			last_node = last_node->next;
-		last_node = last_node->new_node
+
+		last_node->next = new_node;
 	}
 	return (*head);
 }
