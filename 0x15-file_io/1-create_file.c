e3a2b76b84394ed8bd26a51c43202f9aa4c7222d
torpman <torpman1112@gmail.com>
2022-05-02 17:18:31 -0700

    errors fiexed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,7 @@
 * create_file - This function creates a file
 *
 * @filename: The name of the file to create
-* @content: The content of the file
+* @text_content: The content of the file
 *
 * Return: return 1 on success and -1 on various failures
 */
@@ -18,6 +18,9 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
+	if (!text_content)
+		text_content = "";
+
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
@@ -26,8 +29,10 @@ int create_file(const char *filename, char *text_content)
 		;
 
 	nwr = write(fd, text_content, tx_cnt + 1);
+	if (nwr == -1)
+		return (-1);
 
-	close (nwr);
+	close(nwr);
 
-	return (1);	
+	return (1);
 }
