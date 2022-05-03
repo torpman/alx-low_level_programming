8f69e19420ff8726bb88b3dfee3e36e623820b28
torpman <torpman1112@gmail.com>
2022-05-02 17:34:22 -0700

    header file error fixed

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,8 +4,9 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include "holberton.h"
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 
 ssize_t read_textfile(const char *filename, size_t letters);
